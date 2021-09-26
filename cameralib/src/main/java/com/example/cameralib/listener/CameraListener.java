23f6b8e302f4b6e2674d1776183ee7f361d04746
tangdanyu <15698135927@163.com>
2021-09-26 17:30:39 +0800

    camera2 return Y U V

--- cameralib/src/main/java/com/example/cameralib/listener/CameraListener.java
+++ cameralib/src/main/java/com/example/cameralib/listener/CameraListener.java
@@ -2,16 +2,24 @@ package com.example.cameralib.listener;
 
 
 import android.graphics.Bitmap;
+import android.util.Size;
 
 public interface CameraListener {
     //相机启动
-    void onCameraOpened(int width, int height,int displayOrientation);
-    //相机关闭
-    default void onCameraClosed(){}
-    //相机数据
-    void onCameraPreview(byte[] data, int width, int height , int displayOrientation);
-    //拍照
-    default void onPictureTaken(byte[] data){}
+    void onCameraOpened(int width, int height, int displayOrientation);
 
-    default void onBitmap(Bitmap bitmap,int displayOrientation){}
+    //相机关闭
+    default  void onCameraClosed(){}
+
+    //相机数据
+    void onCameraPreview(byte[] data, int width, int height, int displayOrientation);
+
+    default void onPreview(byte[] y, byte[] u, byte[] v, Size previewSize, int stride) {
+    }
+
+    default void onPictureTaken(byte[] data) {
+    }
+
+    default void onBitmap(Bitmap bitmap, int displayOrientation) {
+    }
 }
