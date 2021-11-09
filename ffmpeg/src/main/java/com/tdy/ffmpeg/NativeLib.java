e6646f9672de33da4bc59ad5de3b0abc874e5c6c
tangdanyu <15698135927@163.com>
2021-11-09 17:11:21 +0800

    新增ffmpeg库

---
+++ ffmpeg/src/main/java/com/tdy/ffmpeg/NativeLib.java
@@ -0,0 +1,23 @@
+package com.tdy.ffmpeg;
+
+public class NativeLib {
+
+    // Used to load the 'ffmpeg' library on application startup.
+
+    static {
+        System.loadLibrary("native-lib");
+        System.loadLibrary("avutil");
+        System.loadLibrary("swresample");
+        System.loadLibrary("avcodec");
+        System.loadLibrary("avformat");
+        System.loadLibrary("swscale");
+        System.loadLibrary("avfilter");
+        System.loadLibrary("avdevice");
+    }
+
+    /**
+     * A native method that is implemented by the 'ffmpeg' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
